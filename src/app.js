207455631c2e1007cfaaf0841d34a27ec5504b93
Trong Vu <2251120132@ut.edu.vn>
2026-04-29 23:16:17 +0700

    init source

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Server thuê xe đang chạy");
+});
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
